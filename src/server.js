73e2e278bd8080fc9b564c67e58110e31a99627c
Vivin.M <vivinmani20@gmail.com>
2026-04-18 16:46:55 +0530

    main

--- src/server.js
+++ src/server.js
@@ -15,7 +15,8 @@ if (!process.env.JWT_SECRET) {
 
 app.use(cors());
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const employeeRoutes = require('./routes/employeeRoutes');
 const authRoutes = require('./routes/authRoutes');

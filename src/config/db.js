f3c9d5fc996875b6d262b9843d774dec5434616e
Vivin.M <vivinmani20@gmail.com>
2026-04-30 19:38:16 +0530

    main

--- src/config/db.js
+++ src/config/db.js
@@ -18,7 +18,8 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  timezone: '+00:00'
 });
 
 module.exports = pool.promise();
